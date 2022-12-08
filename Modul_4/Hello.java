f67d50bbeac844ee25509e1ea4cceb1191165aae
elvisusanti20 <114349571+elvisusanti20@users.noreply.github.com>
2022-12-08 12:23:13 +0700

    Add files via upload

---
+++ Modul_4/Hello.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul_4;
+
+/**
+ *
+ * @author susan
+ */
+public class Hello
+{
+ /**
+ * My first java program
+ */
+ public static void main(String[] args) {
+ //menampilkan stringâ€?Hello worldâ€? pada screen
+
+ System.out.println("Hello world!");
+ }
+} 
