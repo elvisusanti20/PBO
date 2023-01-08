52078e1b2f8c0e2090de06a21e3ade7250e6610a
elvisusanti20 <114349571+elvisusanti20@users.noreply.github.com>
2023-01-08 21:42:17 +0700

    Update PengembalianDaoImpl.java

--- Praktek PBO/susan/model/PengembalianDaoImpl.java
+++ Praktek PBO/susan/model/PengembalianDaoImpl.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 /**
  *
- * @author Lenovo
+ * @author susan
  */
 public class PengembalianDaoImpl implements PengembalianDao{
     List<Pengembalian> data= new ArrayList<>();
